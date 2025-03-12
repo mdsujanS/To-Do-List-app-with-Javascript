249aca25bdbd1cd3f6a4b7b30043ef8632858e12
Md Sujan Mia <mdsujanmia860@gmail.com>
2025-03-13 00:14:16 +0600

    Create delete / Update function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-// document.addEventListener('DOMContentLoaded', loadTask);
+document.addEventListener('DOMContentLoaded', loadTask);
 
-// function loadTask(){
-//     let task = JSON.parse(localStorage.getItem('tasks')) || [];
-//     task.foreach(ta => addTaskToDome(ta));
-// }
+function loadTask(){
+    let task = JSON.parse(localStorage.getItem('tasks')) || [];
+    task.foreach(ta => addTaskToDome(ta));
+}
 
 function addTask(){
     let inputText = document.getElementById('inputTask');
@@ -32,8 +32,11 @@ function addTaskToDome(input_text){
     console.log(ul);
     let li = document.createElement('li');
     li.innerHTML = `
-        <span> ${input_text}</span>;
-        <span><button onclick="deleteTask(this)">Submit</button></span>
+        <span> ${input_text}</span>
+        <span>
+        <button onclick="deleteTask(this)" class="Deletebutton">Delete</button>
+        <button onclick="UpdateTask(this)" class="Updatebutton">Update</button>
+        </span>
     `
     ul.appendChild(li);
 
@@ -48,5 +51,27 @@ function deleteTask(element){
     let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     tasks_filter = tasks.filter(task => task !== text);
     localStorage.setItem("tasks", JSON.stringify(tasks_filter));
+
+}
+
+function UpdateTask(element){
+    let li = element.parentElement.parentElement;
+    let child_li  = li.firstElementChild;
+    let current_text = child_li.innertext;
+
+    let new_text = prompt('Update Task: ', current_text);
+
+    if (new_text !== null){
+        child_li.innerText = new_text;
+
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        let index_current_text = tasks.indexOf(current_text);
+
+        tasks[index_current_text] = new_text;
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+
+    }
+
 
 }
